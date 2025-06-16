8c69c1c4da5f4e4fb7ab0a7d1fa745077524f86a
Estarlin R <dev@estarlincito.com>
2025-06-16 01:32:44 -0400

    tyne@1.0.0

--- src/core/infer.ts
+++ src/core/infer.ts
@@ -1,3 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { TyneType } from './tyne.js';
+
 export type infer<T extends TyneType<any>> = T['_type'];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// eslint-disable-next-line simple-import-sort/exports
 export * as t from './core/index.js';
+export type { TyneType, TyneDefault, TyneOptional } from './core/tyne.js';
+export type { TyneArray } from './core/array.js';
+export type { TyneBoolean } from './core/boolean.js';
+export type { TyneNumber } from './core/number.js';
+export type { TyneObject } from './core/object.js';
+export type { TyneString } from './core/string.js';
+export type { TyneUnion } from './core/union.js';
